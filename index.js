fe797a44ed61ea0978fc8fbf81af36289e033ab7
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-07 18:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 /* Your code goes here */
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'bob';
+  const favoriteCustomer = 'joe';
+};
